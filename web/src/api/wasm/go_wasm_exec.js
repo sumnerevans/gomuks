9df42aa73bd5ae0e5139e3b65986150713274e47
Tulir Asokan <tulir@maunium.net>
2025-08-17 20:28:42 +0300

    web/wasm: remove debug log from upstream wasm_exec.js

--- web/src/api/wasm/go_wasm_exec.js
+++ web/src/api/wasm/go_wasm_exec.js
@@ -19,7 +19,6 @@
 				outputBuf += decoder.decode(buf);
 				const nl = outputBuf.lastIndexOf("\n");
 				if (nl != -1) {
-					console.log(outputBuf.substring(0, nl));
 					outputBuf = outputBuf.substring(nl + 1);
 				}
 				return buf.length;
