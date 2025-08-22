bdca4abbc8cba4d4bcc91d3b55f199b9f5c33a7f
Tulir Asokan <tulir@maunium.net>
2025-08-23 01:48:55 +0300

    Revert "ci: move build-wasm.sh to top level"
    This reverts commit 6d8af2f7b79ab9ef3b995c897ea4c1518cf8d195.

--- web/build-wasm.sh
+++ web/build-wasm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-cd $(dirname $0)
-export MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | head -n1 | awk '{ print $2 }')
+export MAUTRIX_VERSION=$(cat ../go.mod | grep 'maunium.net/go/mautrix ' | head -n1 | awk '{ print $2 }')
 export GOOS=js
 export GOARCH=wasm
 GO_LDFLAGS="-X go.mau.fi/gomuks/version.Tag=$(git describe --exact-match --tags 2>/dev/null) -X go.mau.fi/gomuks/version.Commit=$(git rev-parse HEAD) -X 'go.mau.fi/gomuks/version.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
-go build -ldflags "$GO_LDFLAGS" -o web/src/api/wasm/_gomuks.wasm -tags goolm ./cmd/wasmuks || exit 2
+go build -ldflags "$GO_LDFLAGS" -o src/api/wasm/_gomuks.wasm -tags goolm ../cmd/wasmuks || exit 2
